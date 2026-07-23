61121787d666c1327d74270e157fc288201be420
Gr8BubbyFish1 <lukepalios@gmail.com>
2026-07-23 01:58:27 -0400

    guess who forgot to add a sample scene haha
    also don't forget but each new scene needs to be addded to the scene list so it actually loads

--- Assets/Scripts/UI/TitleController.cs
+++ Assets/Scripts/UI/TitleController.cs
@@ -5,14 +5,6 @@ using UnityEngine.SceneManagement;
 public class TitleController : MonoBehaviour
 {
     
-    [SerializeField] private String spring = "hello";
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log(spring);
-    }
-    
     public void SendToScene(string sceneName)
     {
         Debug.Log("Sending to scene: " + sceneName);
