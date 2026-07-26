85ce5821e1d185c99980b8e194e3302cf68eeabb
Gr8BubbyFish1 <lukepalios@gmail.com>
2026-07-26 03:21:15 -0400

    Modernized!
    Added in Gabi's (and Joey's) sprites and changed up the gameplay so it fits the theme.

--- Assets/Scripts/Tapper/BagBehavior.cs
+++ Assets/Scripts/Tapper/BagBehavior.cs
@@ -3,32 +3,12 @@ using UnityEngine;
 
 public class BagBehavior : MonoBehaviour
 {
-    public TapperController tapperController;
-    
     public TapperController.Blood bloodType;
-    public TapperController.Blood belt;
-    [SerializeField] private float scoringZone;
-
-    // nothin to see here just standard debug statements
-    // private void Start()
-    // {
-    //     Debug.Log($"wheeee I'm a lovely little {bloodType} bag going for a lovely walk down {belt} street");
-    // }
+    //this... this really doesn't need to be its own script, does it?
 
     private void Update()
     {
-        if (transform.position.x < scoringZone)
-        {
-            if (bloodType == belt)
-            {
-                //play score sfx
-            }
-            else
-            {
-                tapperController.LoseLife();
-            }
-            
+        if (transform.position.x < -10)
             Destroy(gameObject);
-        }
     }
 }
--- Assets/Scripts/Tapper/TapperController.cs
+++ Assets/Scripts/Tapper/TapperController.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TapperController : MonoBehaviour
 {
     public enum Blood
     {
-        Plasma,
-        RedBlood,
-        WhiteBlood,
-        Platelets
+        A,
+        B,
+        O,
     }
     
     [SerializeField] private GameObject player;
@@ -19,22 +20,36 @@ public class TapperController : MonoBehaviour
 
     [SerializeField] private float totalServings;
     // [SerializeField] private GameObject[] lane; // I don't think I ever use this? why is this here?
+    
+    [SerializeField] private TextMeshProUGUI ScoreText;
 
     private void Start()
     {
         List<Blood> bloodFeed = new List<Blood>();
         for (int i = 0; i < totalServings; i++)
         {
-            bloodFeed.Add((Blood)Random.Range(0,4));
+            bloodFeed.Add((Blood)Random.Range(0,3));
         }
         Debug.Log(bloodFeed.Count);
         feedController.bloodFeed = bloodFeed;
-        feedController.BuildBloodFeed();
+        
+        ScoreText.text = $"Served:\n0/{totalServings}";
     }
 
-    public void ServeMug(TapperController.Blood currentBelt)
+    private void Update()
     {
-        Blood nextServe = feedController.RemoveBlood();
+        //This is terrible practice, but I have mangled this minigame's code enough to go back now. 
+        ScoreText.text = $"Served:\n {totalServings - feedController.bloodFeed.Count}/{totalServings}";
+    }
+
+    public void ServeMug(Blood currentBelt, Blood? mug)
+    {
+        if (mug == null)
+        {
+            Debug.LogError("You sent a mug full of nothin somehow");
+            return;
+        }
+        Blood nextServe = (Blood)mug;
         //summon a new bag 2 units left of the player then send it alooooong
         GameObject newBag = Instantiate(bloodBagPrefab[(int)nextServe], null, true);
         newBag.transform.parent = this.transform;
@@ -42,18 +57,19 @@ public class TapperController : MonoBehaviour
         newBag.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(-bagSpeed, 0);
 
         BagBehavior bag = newBag.GetComponent<BagBehavior>();
-        bag.tapperController = this;
         bag.bloodType = nextServe;
-        bag.belt = currentBelt;
     }
 
     public void LoseLife()
     { 
+        StaticManager.removeLife();
+        StaticManager.EndMiniGame();
         Debug.Log("Damn man you fucking suck");
     }
 
     public void GameWon()
     {
         Debug.Log("congarts champ");
+        StaticManager.EndMiniGame();
     }
 }
